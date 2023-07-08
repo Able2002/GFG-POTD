bbdb665ff93daf086c79a883db169ed40bdfcc05
Able P Abraham <95495989+Able2002@users.noreply.github.com>
2023-07-09 01:23:20 +0530

    July 9

---
+++ July 2023/Smallest Positive missing number.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+       vector<bool>a(n+1,false);
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<0 || arr[i]>n){
+                continue;
+            }
+            a[arr[i]]=true;
+        }
+        for(int i=1;i<=n;i++)
+            if(a[i]==false)
+                return i;
+        return n+1;
+    } 
+};
