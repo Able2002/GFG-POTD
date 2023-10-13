fb2459d62dc81cfa639350075c2842a8d40c5c60
Able P Abraham <95495989+Able2002@users.noreply.github.com>
2023-10-13 10:11:09 +0530

    Create Floor in Bst.cpp

---
+++ October 2023/Floor in Bst.cpp
@@ -0,0 +1,22 @@
+class Solution{
+
+public:
+    
+    vector<int>store;
+    void inorder(Node*root){
+        if(root!=NULL){
+            inorder(root->left);
+            store.push_back(root->data);
+            inorder(root->right);
+        }
+    }
+    
+    int floor(Node* root, int x) {
+        inorder(root);
+        if(x<store[0])return -1;
+        
+        int index = lower_bound(store.begin(),store.end(),x)-store.begin();
+        if(store[index]!=x)return store[index-1];
+        return store[index];
+    }
+};
