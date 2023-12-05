cb5a70fda5e2ca5c050ad2a715c438fe71ebdf1e
Able P Abraham <95495989+Able2002@users.noreply.github.com>
2023-12-06 00:33:42 +0530

    06 December 2023

---
+++ December 2023/How Many X's?.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countX(int L, int R, int X) {
+        int count = 0;
+        for (int a = L + 1; a < R; a++) {
+            int temp = a; // Temporary variable to extract digits
+            while (temp > 0) {
+                int lastDigit = temp % 10; // found the last digit
+                if (lastDigit == X) {
+                    count++;
+                }
+                temp /= 10; // reduce the digit 2 digit tha to ek digit hoga
+            }
+        }
+        return count;
+    }
+};
